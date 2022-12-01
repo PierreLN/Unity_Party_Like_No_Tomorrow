a3f273602933bdcac7647055a567f6a57e7385e6
Pierre Long Nguyen <pierrelongnguyen@gmail.com>
2022-12-01 12:45:39 -0500

    correction

--- Assets/Script/PageIntro.cs
+++ Assets/Script/PageIntro.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 public class PageIntro : MonoBehaviour
 {
     public GameObject msgBox;
-    public string nextScene;
+    private string nextScene = "Level_1";
+
+    public GameObject fondu_intro;
+    private SpriteRenderer fondu_render;
+    private float fadeSpeed = 0.0f;
+    private Color couleur;
+
 
     public void starting() 
     {
-        SceneManager.LoadScene(nextScene);
+        StartCoroutine(change_scene());
     }
 
     public void quitting() 
@@ -20,7 +26,16 @@ public class PageIntro : MonoBehaviour
 
     private void Start()
     {
+        fondu_render = fondu_intro.GetComponent<SpriteRenderer>();
+        couleur = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+
         StartCoroutine(msg_box());
+    }
+
+    private void Update()
+    {
+        couleur.a += fadeSpeed;
+        fondu_render.color = couleur;
     }
 
     IEnumerator msg_box()
@@ -32,6 +47,16 @@ public class PageIntro : MonoBehaviour
             msgBox.SetActive(true);
             yield return new WaitForSeconds(5.1f);
             msgBox.SetActive(false);
+        }
+    }
+
+    IEnumerator change_scene()
+    {
+        while (true)
+        {
+            fadeSpeed = 0.005f;
+            yield return new WaitForSeconds(3.0f);
+            SceneManager.LoadScene(nextScene);
         }
     }
 
